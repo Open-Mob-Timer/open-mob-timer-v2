79df00ef7b4e6ad78d38a81f5ce787a4dc74b65d
Daniel Breen <daniel.breen@kno2.com>
2022-01-30 22:07:54 -0600

    Remove Home Module

--- apps/web/src/app/app.module.ts
+++ apps/web/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
 import { AppRoutingModule } from './app-routing.module';
-import { WebFeatureHomeModule } from '@omt/web-feature-home';
 import { CreateMobModule } from '@omt/web/feature/create-mob';
 
 @NgModule({
