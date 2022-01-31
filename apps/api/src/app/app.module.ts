b9f5077e0bf8133c23ac7677085b7921d2003325
Daniel Breen <daniel.breen@kno2.com>
2022-01-31 08:42:57 -0600

    Connect to PostgreSQL Database

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-
+import { CoreModule } from './@core/core.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [CoreModule],
+    controllers: [AppController],
+    providers: [AppService]
 })
 export class AppModule {}
