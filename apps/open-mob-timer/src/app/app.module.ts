8afbc966a2ad9960e60d8f0ac20516af601bb5dc
Daniel Breen <daniel.breen@kno2.com>
2022-01-30 20:21:06 -0600

    Install Nebular

--- apps/open-mob-timer/src/app/app.module.ts
+++ apps/open-mob-timer/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbThemeModule, NbLayoutModule, NbIconModule, NbButtonModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule, NbThemeModule.forRoot({ name: 'default' }), NbLayoutModule, NbIconModule, NbButtonModule, NbEvaIconsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
