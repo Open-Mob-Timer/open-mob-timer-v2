e042565d9dd26c0eb49176c62154dd53c243cbb3
Daniel Breen <daninreallife7@gmail.com>
2022-02-05 20:22:30 -0600

    Run nx format

--- libs/web/mob/src/lib/mob.component.spec.ts
+++ libs/web/mob/src/lib/mob.component.spec.ts
@@ -3,23 +3,22 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MobComponent } from './mob.component';
 
 describe('MobComponent', () => {
-  let component: MobComponent;
-  let fixture: ComponentFixture<MobComponent>;
+    let component: MobComponent;
+    let fixture: ComponentFixture<MobComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ MobComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [MobComponent]
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(MobComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(MobComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
