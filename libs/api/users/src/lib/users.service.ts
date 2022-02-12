9ae4aad9c15e08a93238c0cd2fb7ee3b423961df
Daniel Breen <daninreallife7@gmail.com>
2022-02-11 21:20:53 -0600

    Remove Encapsulated Entities

--- libs/api/users/src/lib/users.service.ts
+++ libs/api/users/src/lib/users.service.ts
@@ -1,7 +1,7 @@
+import { Repository } from 'typeorm';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { User } from './entities/user.entity';
+import { User } from '@omt/api/common';
 
 @Injectable()
 export class UsersService {
