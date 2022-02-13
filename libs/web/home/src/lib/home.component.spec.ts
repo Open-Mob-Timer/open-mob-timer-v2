a653637e5eea4273693d5cb46e6c6b2268a08f4d
Daniel Breen <daninreallife7@gmail.com>
2022-02-12 20:45:43 -0600

    Fix Home Component Tests

--- libs/web/home/src/lib/home.component.spec.ts
+++ libs/web/home/src/lib/home.component.spec.ts
@@ -1,25 +1,44 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { NbButtonModule, NbFormFieldModule, NbIconModule, NbInputModule, NbThemeModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { MobsService } from '@omt/web/data-access';
+import { CreateMobComponent } from './components/create-mob/create-mob.component';
 
 import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
+    let component: HomeComponent;
+    let fixture: ComponentFixture<HomeComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [HomeComponent, CreateMobComponent],
+            imports: [
+                FormsModule,
+                NbThemeModule.forRoot(),
+                NbButtonModule,
+                NbFormFieldModule,
+                NbIconModule,
+                NbEvaIconsModule,
+                NbInputModule
+            ],
+            providers: [
+                {
+                    provide: MobsService,
+                    useValue: {}
+                }
+            ]
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(HomeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(HomeComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
