aee259d0072671e814bc5231c045eab218269253
Daniel Breen <daninreallife7@gmail.com>
2022-02-13 00:45:27 -0600

    Combine Into Single App

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -5,9 +5,18 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from '@omt/api/users';
 import { ORM_CONFIG } from '../../ormconfig';
 import { MobsModule } from '@omt/api/mobs';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-    imports: [TypeOrmModule.forRoot(ORM_CONFIG), UsersModule, MobsModule],
+    imports: [
+        TypeOrmModule.forRoot(ORM_CONFIG),
+        UsersModule,
+        MobsModule,
+        ServeStaticModule.forRoot({
+            rootPath: join(__dirname, '..', 'web')
+        })
+    ],
     controllers: [AppController],
     providers: [AppService]
 })
