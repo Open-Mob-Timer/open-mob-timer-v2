0ff96624130d3f489e748179b13d43d521342c9f
Daniel Breen <daninreallife7@gmail.com>
2022-02-13 17:06:15 -0600

    WIP Production Configuration

--- apps/api/ormconfig.ts
+++ apps/api/ormconfig.ts
@@ -4,10 +4,10 @@ import { Mob, User } from '@omt/api/common';
 // TODO: Setup Configuration Service with .env
 export const ORM_CONFIG: TypeOrmModuleOptions = {
     type: 'postgres',
-    host: 'localhost',
+    host: process.env.TYPEORM_HOST || 'localhost',
     port: 5432,
-    username: 'postgres',
-    password: 'postgres',
-    database: 'mobtimer',
+    username: process.env.TYPEORM_USERNAME || 'postgres',
+    password: process.env.TYPEORM_PASSWORD || 'postgres',
+    database: process.env.TYPEORM_DATABASE || 'mobtimer',
     entities: [Mob, User]
 };
